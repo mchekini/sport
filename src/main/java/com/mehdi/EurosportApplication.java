3b475cd3c6a0ecce3783268aadb9c26ae72645c6
mchekini <m_chekini@esi.dz>
2017-07-13 23:56:50 +0200

    init project

---
+++ src/main/java/com/mehdi/EurosportApplication.java
@@ -0,0 +1,12 @@
+package com.mehdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurosportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurosportApplication.class, args);
+	}
+}
